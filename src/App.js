8a5c66c63b1271a213db11f589dc3af186e8d57c
AkhilNimmathota <118796965+AkhilNimmathota@users.noreply.github.com>
2024-07-21 15:07:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import LoveLetter from './components/letter/letter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LoveLetter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/letter/letter.js
@@ -0,0 +1,48 @@
+import React, { useState, useRef } from 'react';
+import './letter.css';
+import audioFile from './bgm.mp3';
+
+const LoveLetter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullSize, setIsFullSize] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleOpenLetter = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsFullSize(true);
+    
+      if (audioRef.current) {
+        audioRef.current.play()
+          .then(() => console.log("Playback succeeded"))
+          .catch(e => console.error("Playback failed:", e));
+      }
+    }, 800);
+  };
+
+  const handleCloseLetter = () => {
+    setIsFullSize(false);
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+      setIsOpen(false);
+    }, 800);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
+        mah dear chinnari🖤,<br />
+        never have i been so blessed as to fall in love with someone as wonderful as you...<br />i loveeee youuuu :) forever and ever and ever<br />
+        always yours,<br />
+        @prabha
+      </div>
+      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
+  );
+};
+
+export default LoveLetter;
